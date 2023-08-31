12c3e61d2e18edf0404d6232c7462fdea93895ee
Kamal El alami <kelalami123@gmail.com>
2023-08-31 13:50:07 -0700

    It was bad idea

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,22 +9,8 @@
 
 void print_binary(unsigned long int n)
 {
-	int bit = sizeof(n) * 8, printed = 0;
+	if (n > 1)
+		print_binary(n >> 1);
 
-	while (bit)
-	{
-		if (n & 1L << --bit)
-		{
-			_putchar('1');
-			printed++;
-		}
-		else if (printed)
-		{
-			_putchar('0');
-		}
-	}
-	if (!printed)
-	{
-		_putchar('0');
-	}
+	_putchar((n & 1) + '0');
 }
